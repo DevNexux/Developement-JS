8e5fafc41a126c32fcf2e728888ae023e8c95398
DevNexux <katharv720@gmail.com>
2026-09-21 12:46:02 +0000

    Variables and Constants

---
+++ JS_Basics/Variable_Const.js
@@ -0,0 +1,10 @@
+const employeeId = 101
+let employeeEmail="hiiiii25@gmail.com"
+var pass="1234HjC"
+employeeCity="Nagpur"
+let employeestate;
+
+// employeeId=102 not allowed 
+
+// use let keyword to define a variable because var keyword has issue with block scope and functional scope
+console.table([employeeId,employeeEmail,pass,employeeCity,employeestate])
