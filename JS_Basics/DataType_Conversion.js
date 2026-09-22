ba4a93cff897d1e2c882a4ee87a0476a4ac44247
DevNexux <katharv720@gmail.com>
2026-09-22 17:39:46 +0000

    DataType Conversion

---
+++ JS_Basics/DataType_Conversion.js
@@ -0,0 +1,21 @@
+let number=23
+console.log(typeof number)
+let ConvertString=String(number)
+console.log(ConvertString)
+console.log(typeof ConvertString)
+
+let isLoggedin=1
+console.log(typeof isLoggedin)
+let ConvertBoolean=Boolean(isLoggedin)
+console.log(ConvertBoolean);
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let str ="33abs"
+let num=Number(str)
+console.log(typeof num)
+console.log(num);
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
